5c958688fa1dd0146c84192fd7a3a5badedb7812
Fabricio Milanez <fabriciomilanez@protonmail.com>
2022-04-05 15:41:33 -0300

    modified command line parser logic to accomodate new usage, updated help

--- sources/Projeto/src/main.cpp
+++ sources/Projeto/src/main.cpp
@@ -10,113 +10,122 @@ namespace fs = std::filesystem; // filesystem namespace alias
 #include <cstdio> 	// fprintf
 #include <cstddef> 	// size_t
 #include <cstring> 	// strncmp
-#include <cstdlib>	// strtol, exit
+#include <cstdlib>	// strtof, exit
 
 // internos
 #include <PointCloud.h>
-// #include <SurfaceReconstruction.h>
 
-/********** Variáveis de controle **********/
+/********** Lógica de Controle **********/
 
 namespace ctrl {
 
-// input/output paths
-fs::path input_path{}; 				/* Caminho até diretório do dataset (sparse) ou do arquivo .sfm (dense). */
+// índices dos argumentos da linha de comando na lista de argumentos
+const int _INPUT_PATH_IDX 	= 1; 	/** Índice do argumento orbigatório <input_path> na lista de argumentos. */
+const int _FOCAL_LENGTH_IDX = 2; 	/** Índice do argumento orbigatório <f> na lista de argumentos. */
+const int _PRINCIPAL_X_IDX 	= 3; 	/** Índice do argumento orbigatório <cx> na lista de argumentos. */
+const int _PRINCIPAL_Y_IDX 	= 4; 	/** Índice do argumento orbigatório <cy> na lista de argumentos. */
+const int _MIN_ARGS 		= 5; 	/** Número mínimo de argumentos, também representa o índice do primeiro argumento subsequente aos obrigatórios. */
+
+// macros
+#define ARG_INPUT_PATH 		argv[ctrl::_INPUT_PATH_IDX]
+#define ARG_FOCAL_LENGTH 	argv[ctrl::_FOCAL_LENGTH_IDX]
+#define ARG_CX 				argv[ctrl::_PRINCIPAL_X_IDX]
+#define ARG_CY 				argv[ctrl::_PRINCIPAL_Y_IDX]
+
+// function-like macros
+#define MSG_AND_EXIT_FAILURE(msg, ...) std::fprintf(stderr, msg __VA_OPT__(,) __VA_ARGS__); std::exit(-1);
+#define MSG_AND_EXIT_SUCCESS(msg, ...) std::fprintf(stdout, msg __VA_OPT__(,) __VA_ARGS__); std::exit(0);
+
+// variáveis de conrtrole: caminhos de entrada e saída
+fs::path input_path{}; 				/* Caminho até o arquivo que lista os caminhos das imagens do dataset. */
 fs::path output_path{"./output/"}; 	/* Caminho até o diretório de saída. */
 
-// [--count]
-long int count{0}; /* Número máximo de imagens que devem ser usadas (sparse). */
+// variáveis de controle: parâmetros intrínsecos da câmera
+float f{}; 			/** Distância focal da camera. */
+float cx{}, cy{}; 	/** Offset do ponto principal (x, v) da câmera. */
 
-// [--dense]
-bool compute_dense{false}; /* Flag -- indica se deve computar a nuvem densa ao invés da esparsa. */
+// funções
+
+void help(std::ostream& os = std::cout);
+void parse(const int argc, const char* const* argv);
 
 } // namespace ctrl
 
 /********** Funções **********/
 
-/* Exibe a mensagem de ajuda do programa. */
-void printHelp() {
-	std::cout << 
-		"Uso: ./Projeto <input_path> [output_path] [OPTIONS]\n\n" 
+/* Insere a mensagem de ajuda do programa num objeto std::ostream. */
+void ctrl::help(std::ostream& os) {
+	os << 
+		"Uso: ./Projeto <input_path> <f> <cx> <cy> [OPTIONS]\n\n" 
 		"<input_path>:\n" 
-		"    Caminho do diretorio contendo o dataset usado para computar a nuvem de\n" 
-		"    pontos esparsa. Ao final da execucao, serao gerados tres arquivos:\n" 
-		"        -`point_cloud.obj`: Arquivo no formato *.obj contendo apenas a lista de\n" 
-		"         vertices do objeto.\n" 
-		"        -`nview_match.nvm`: Arquivo no formato *.nvm contendo as cameras e suas\n" 
-		"         respectivas matrizes intrinsecas e extrinsecas e listas de vertices.\n" 
-		"        -`pose.sfm`: Arquivo no formato *.sfm contendo informacoes sobre a pose\n" 
-		"         estimada de cada camera (matrizes intrinseca e extrinseca e ponto\n" 
-		"         focal).\n" 
-		"[output_path]:\n" 
-		"    Opcional. Caminho ate o diretorio onde serao armazenados os resultados.\n" 
-		"    `./output/`, por padrao\n"
+		"    Arquivo que contem os caminhos ate as imagens a serem usadas para computar a\n" 
+		"    nuvem de pontos esparsa, um por linha. Ao final da execucao, sera gerado um\n" 
+		"    arquivo *.obj contendo somente a lista de vertices representando a nuvem de\n" 
+		"    pontos esparsa." 
 		"OPTIONS:\n" 
 		"    --help, -h:\n" 
-		"        Exibe esta mensagem de ajuda.\n" 
-		"    --count:\n" 
-		"        Especifica o total de imagens do dataset que serao usadas.\n" 
-		"    --dense:\n" 
-		"        Computa a nuvem de pontos densa, usando o diretorio especificado em `input_path`\n" 
-		"        para localizar o arquivo de calibracao das cameras (*.sfm).\n"
+		"        Exibe esta mensagem de ajuda e termina a execucao sem computar nada.\n" 
+		"    -o <output_path>:\n" 
+		"        Caminho ate o diretorio onde serao armazenados os resultados.\n" 
+		"        `$PWD/output/`, por padrao.\n" 
 	<< std::endl;
 }
 
 /* Analisa os argumentos da linha de comando e modifica as variáveis de controle. */
-void parse(int argc, const char* const* argv) {
-
-	// processando flags e argumentos de controle, se existirem
-	for (int arg_idx = 2; arg_idx < argc; ++arg_idx) {
-		const char* current_arg = argv[arg_idx];
-
-		if (std::strncmp(current_arg + 1, "-h", 2) == 0) { 			// --help, -h
-			printHelp();
-			std::exit(0);
-		} else if (std::strncmp(current_arg, "--count", 7) == 0) { 	// --count
-			// incremente arg_idx e verifica se o próximo argumento existe
-			if (current_arg = argv[++arg_idx]; arg_idx < argc) {
-				// tenta converter em um valor inteiro na base10, se falhar, strol retorna 0
-				ctrl::count = std::strtol(current_arg, nullptr, 10);
-			}
-		} else if (std::strncmp(current_arg, "--dense", 7) == 0) { 	// --dense
-			// set flag compute_dense
-			ctrl::compute_dense = true;
-		}
-	}
+void ctrl::parse(const int argc, const char* const* argv) {
 
 	// verificando número mínimo de argumentos
-	if (argc < 2) {
-		printHelp();
+	if (argc < ctrl::_MIN_ARGS) {
+		ctrl::help();
 		std::exit(-1);
 	}
 
-	// extraíndo e validando argumento obrigatório input_path
-	ctrl::input_path = argv[1];
-	if (!fs::exists(ctrl::input_path) || !fs::is_directory(ctrl::input_path)) {
-		std::cout << "[ERRO] Caminho de entrada especificado nao existe ou nao e valido\n";
-		std::exit(-1);
+	// extraindo e validando argumento obrigatório <input_path>
+	if (ctrl::input_path = ARG_INPUT_PATH; !fs::exists(ctrl::input_path) || !fs::is_regular_file(ctrl::input_path)) {
+		MSG_AND_EXIT_FAILURE("[ERRO] Arquivo de entrada `%s` nao existe ou nao e valido\n", ARG_INPUT_PATH)
 	}
 
-	// extraíndo argumento opcional output_path, se existir
-	if (argc > 2) {
-		// validação nas funções que exportam os resultados
-		ctrl::output_path = argv[2];
+	// extraindo e validando argumento obrigatório <f>
+	if (ctrl::f = std::strtof(ARG_FOCAL_LENGTH, nullptr); ctrl::f == 0) {
+		MSG_AND_EXIT_FAILURE("[ERRO] Distancia focal `%s` e invalida\n", ARG_FOCAL_LENGTH)
+	}
+
+	// extraindo e validando argumentos obrigatórios <cx> e <cy>
+	if (ctrl::cx = std::strtof(ARG_CX, nullptr), ctrl::cy = std::strtof(ARG_CY, nullptr); ctrl::cx == 0 || ctrl::cy == 0) {
+		MSG_AND_EXIT_FAILURE("[ERRO] Ponto principal (%s, %s) e invalido\n", ARG_CX, ARG_CY)
+	}
+
+	// verificando e extraindo argumentos opcionais, se existirem
+	for (int arg_idx = ctrl::_MIN_ARGS; arg_idx < argc; ++arg_idx) { // intervalo da busca: [_MIN_ARGS, argc)
+		const char* current_arg = argv[arg_idx];
+		if (std::strncmp(current_arg + 1, "-h", 2) == 0) { 		// strings que correspondem ao padrão `?-h*`
+			ctrl::help();
+			std::exit(0);
+		} else if (std::strncmp(current_arg, "-o", 2) == 0) { 	// -o <output_path>
+			// incremente arg_idx e verifica se o próximo argumento existe
+			if (current_arg = argv[++arg_idx]; arg_idx < argc) {
+				// validação do diretório é feita nas funções que exportam os resultados
+				/**
+				 * FIXME:
+				 * É possível que `current_arg` não seja um caminho, mas sim outro argumento 
+				 * opcional passado pelo usuário (no caso `... -o -h`, por exemplo). Neste caso o
+				 * argumento opcional que segue `-o` será ignorado e o programa vai tentar usar 
+				 * este argumento como diretório de saída e pode causar um erro durante a execução.
+				*/
+				ctrl::output_path = current_arg;
+			} else {
+				std::cout << "[INFO]: Nenhum valor foi passado para a opcao `-o`, usando `./output/` como caminho de saida\n";
+				ctrl::output_path = "./output/";
+			}
+		}
 	}
 }
 
 // main entry point
-// build/Projeto/Projeto ~/dev/cpp/ProjetoGraduacao/datasets/gargoyle ~/dev/cpp/ProjetoGraduacao/output
+// build/Projeto/Projeto ~/dev/cpp/ProjetoGraduacao/datasets/gargoyle/00_index.txt <f> <cx> <cy> -o ~/dev/cpp/ProjetoGraduacao/output
 int main(int argc, char** argv) {
-	
-	// analisa os argumentos da linha de comando
-	parse(argc, argv);
 
-	// instanciando objeto PointCloud sem nome e computando nuvem de pontos
-	PointCloud{ 
-		ctrl::input_path.make_preferred(), 		// std::filesystem::path
-		ctrl::output_path.make_preferred(), 	// std::filesystem::path
-		static_cast<std::size_t>(ctrl::count) 	// std::size_t
-	}.computeSparse();
+	ctrl::parse(argc, argv);
 
 	return 0;
 }
