7030e6bba7ed7c8e803fc5ae37849560ee5df6fc
Fabricio Milanez <fabriciomilanez@protonmail.com>
2022-04-07 18:28:17 -0300

    refactored cmd line parsing logic to fix an issue with redeclared symbols across multiple files

--- sources/Projeto/include/cmdline_parser.h
+++ sources/Projeto/include/cmdline_parser.h
@@ -9,45 +9,33 @@
 
 namespace fs = std::filesystem; // filesystem namespace alias
 
-// libc
-#include <cstdio> 	// fprintf, stdout, stderr, EXIT_FAILURE, EXIT_SUCCESS
-#include <cstdlib> 	// exit
-
 /********** cmdline_parser.h **********/
 
 /** Lógica de Controle. */
 namespace ctrl {
 
-// índices dos argumentos da linha de comando na lista de argumentos
-const int ARG_INPUT_FILE_IDX 	= 1; /** Índice do argumento orbigatório <input_file> na lista de argumentos. */
-const int ARG_FOCAL_LENGTH_IDX 	= 2; /** Índice do argumento orbigatório <f> na lista de argumentos. */
-const int ARG_PRINCIPAL_X_IDX 	= 3; /** Índice do argumento orbigatório <cx> na lista de argumentos. */
-const int ARG_PRINCIPAL_Y_IDX 	= 4; /** Índice do argumento orbigatório <cy> na lista de argumentos. */
-const int ARG_MINIMUM 			= 5; /** Número mínimo de argumentos, também representa o índice do primeiro argumento subsequente aos obrigatórios. */
+/** Armazena os argumentos validados extraídos da linha de comando em variáveis de controle. */
+struct args {
+	// índices dos argumentos da linha de comando na lista de argumentos
+	static const int INPUT_FILE_IDX 	= 1; 	/** Índice do argumento orbigatório <input_file> na lista de argumentos. */
+	static const int FOCAL_LENGTH_IDX 	= 2; 	/** Índice do argumento orbigatório <f> na lista de argumentos. */
+	static const int PRINCIPAL_X_IDX 	= 3; 	/** Índice do argumento orbigatório <cx> na lista de argumentos. */
+	static const int PRINCIPAL_Y_IDX 	= 4; 	/** Índice do argumento orbigatório <cy> na lista de argumentos. */
+	static const int MIN_ARGS 			= 5; 	/** Número mínimo de argumentos. Também representa o índice do primeiro argumento subsequente aos obrigatórios. */
 
-// macros
-#define ARG_INPUT_FILE 		argv[ctrl::ARG_INPUT_FILE_IDX]
-#define ARG_FOCAL_LENGTH 	argv[ctrl::ARG_FOCAL_LENGTH_IDX]
-#define ARG_CX 				argv[ctrl::ARG_PRINCIPAL_X_IDX]
-#define ARG_CY 				argv[ctrl::ARG_PRINCIPAL_Y_IDX]
+	// caminhos de entrada e saída
+	fs::path input_file{}; 				/* Caminho até o arquivo que lista os caminhos das imagens do dataset. */
+	fs::path output_path{"./output"}; 	/* Caminho até o diretório de saída. */
 
-// function-like macros
-#define log_error_and_exit(msg, ...) 	std::fprintf(stderr, "[ERROR]: " msg __VA_OPT__(,) __VA_ARGS__); std::exit(EXIT_FAILURE)
-#define log_info(msg, ...) 				std::fprintf(stdout, "[INFO]: " msg __VA_OPT__(,) __VA_ARGS__)
-#define display_help_and_exit() 		ctrl::help(); std::exit(EXIT_SUCCESS)
-
-// variáveis de controle: caminhos de entrada e saída
-fs::path input_file{}; 				/* Caminho até o arquivo que lista os caminhos das imagens do dataset. */
-fs::path output_path{"./output/"}; 	/* Caminho até o diretório de saída. */
-
-// variáveis de controle: parâmetros intrínsecos da câmera
-float f{}; 			/** Distância focal da câmera. */
-float cx{}, cy{}; 	/** Offset do ponto principal (x, v) da câmera. */
+	// Parâmetros intrínsecos da câmera
+	float f{}; 			/** Distância focal da câmera. */
+	float cx{}, cy{};	/** Offset do ponto principal (x, v) da câmera. */
+};
 
 // funções (declaração)
 
 void help(std::ostream& os = std::cout);
-void parse(const int argc, const char* const* argv);
+args parse(const int argc, const char* const* argv);
 
 } // namespace ctrl
 
--- sources/Projeto/src/cmdline_parser.cpp
+++ sources/Projeto/src/cmdline_parser.cpp
@@ -7,22 +7,37 @@
 namespace fs = std::filesystem; // filesystem namespace alias
 
 // libc
+#include <cstdio> 	// fprintf, stdout, stderr, EXIT_FAILURE, EXIT_SUCCESS
+#include <cstdlib>	// strtof, exit
 #include <cstring> 	// strncmp
-#include <cstdlib>	// strtof
 
 // internos
 #include <cmdline_parser.h>
 
+/********** Macros **********/
+
+// macros para acesso à índices específicos da lista de argumentos
+#define ARG_INPUT_FILE 		argv[ctrl::args::INPUT_FILE_IDX]
+#define ARG_FOCAL_LENGTH 	argv[ctrl::args::FOCAL_LENGTH_IDX]
+#define ARG_CX 				argv[ctrl::args::PRINCIPAL_X_IDX]
+#define ARG_CY 				argv[ctrl::args::PRINCIPAL_Y_IDX]
+
+// function-like macros
+#define log_error_and_exit(msg, ...) 	std::fprintf(stderr, "[ERROR]: " msg __VA_OPT__(,) __VA_ARGS__); std::exit(EXIT_FAILURE)
+#define log_info(msg, ...) 				std::fprintf(stdout, "[INFO]: " msg __VA_OPT__(,) __VA_ARGS__)
+#define display_help_and_exit() 		ctrl::help(); std::exit(EXIT_SUCCESS)
+
 /********** Funções (definição) **********/
 
-/** Insere a mensagem de ajuda do programa em um objeto std::ostream (stc::cout por padrão). */
+/** Insere a mensagem de ajuda do programa em um objeto std::ostream (std::cout por padrão). */
 void ctrl::help(std::ostream& os) {
 	os << 
 		"Uso: ./Projeto <input_file> <f> <cx> <cy> [OPTIONS]\n" 
 		"    Computa uma nuvem de pontos esparsa para um conjunto de imagens.\n" 
 		"    Ao final da execucao, dois arquivos serao gerados: um arquivo .obj contendo\n" 
-		"    apenas a lista de vertices, representando a nuvem de pontos, e um arquivo .sfm\n" 
-		"    detalhando a pose (matrix de transformacao Rt) estimada para cada camera.\n" 
+		"    apenas a lista de vertices, representando a nuvem de pontos, e um arquivo\n" 
+		"    .sfm detalhando a pose (matrix de transformacao Rt) estimada para cada\n" 
+		"    camera.\n" 
 		"\n" 
 		"<input_file>:\n" 
 		"    Arquivo contendo os caminhos absolutos ate as imagens do dataset a serem\n" 
@@ -42,8 +57,8 @@ void ctrl::help(std::ostream& os) {
 }
 
 /** 
- * Analisa a linha de comando e modifica as variáveis de controle de acordo com 
- * os argumentos presentes.
+ * Analisa a linha de comando e retorna um objeto contendo os argumentos 
+ * extraídos da linha de comando.
  * 
  * Caso um erro de validação seja encontrado, o programa termina a execução com 
  * código de status `EXIT_FAILURE`.
@@ -52,30 +67,33 @@ void ctrl::help(std::ostream& os) {
  * insuficientes foram passados, o programa simplesmente exibe a mensagem de 
  * ajuda e termina a execução com código de status `EXIT_SUCCESS`.
 */
-void ctrl::parse(const int argc, const char* const* argv) {
+ctrl::args ctrl::parse(const int argc, const char* const* argv) {
 
 	// verificando número mínimo de argumentos
-	if (argc < ctrl::ARG_MINIMUM) {
+	if (argc < ctrl::args::MIN_ARGS) {
 		display_help_and_exit();
 	}
 
+	// instanciando variavés de controle com valores padrão
+	ctrl::args extracted{};
+
 	// extraindo e validando argumento obrigatório <input_file>
-	if (ctrl::input_file = ARG_INPUT_FILE; !fs::exists(ctrl::input_file) || !fs::is_regular_file(ctrl::input_file)) {
+	if (extracted.input_file = ARG_INPUT_FILE; !fs::exists(extracted.input_file) || !fs::is_regular_file(extracted.input_file)) {
 		log_error_and_exit("Arquivo de entrada `%s` nao existe ou nao e valido\n", ARG_INPUT_FILE);
 	}
 
 	// extraindo e validando argumento obrigatório <f>
-	if (ctrl::f = std::strtof(ARG_FOCAL_LENGTH, nullptr); ctrl::f == 0) {
+	if (extracted.f = std::strtof(ARG_FOCAL_LENGTH, nullptr); extracted.f == 0) {
 		log_error_and_exit("Distancia focal `%s` e invalida\n", ARG_FOCAL_LENGTH);
 	}
 
 	// extraindo e validando argumentos obrigatórios <cx> e <cy>
-	if (ctrl::cx = std::strtof(ARG_CX, nullptr), ctrl::cy = std::strtof(ARG_CY, nullptr); ctrl::cx == 0 || ctrl::cy == 0) {
+	if (extracted.cx = std::strtof(ARG_CX, nullptr), extracted.cy = std::strtof(ARG_CY, nullptr); extracted.cx == 0 || extracted.cy == 0) {
 		log_error_and_exit("Ponto principal (%s, %s) e invalido\n", ARG_CX, ARG_CY);
 	}
 
 	// verificando e extraindo argumentos opcionais, se existirem
-	for (int arg_idx = ctrl::ARG_MINIMUM; arg_idx < argc; ++arg_idx) { // intervalo da busca: [ARG_MINIMUM, argc)
+	for (int arg_idx = ctrl::args::MIN_ARGS; arg_idx < argc; ++arg_idx) { // intervalo da busca: [MIN_ARGS, argc)
 		const char* current_arg = argv[arg_idx];
 		if (std::strncmp(current_arg + 1, "-h", 2) == 0) { 		// strings que correspondem ao padrão `?-h*`
 			display_help_and_exit();
@@ -90,12 +108,14 @@ void ctrl::parse(const int argc, const char* const* argv) {
 				 * programa ira tentar usar este argumento como o diretório de saída, o que pode
 				 * ocasionar um erro de execução.
 				*/
-				ctrl::output_path = current_arg;
-				log_info("Usando `%s` como caminho de saida\n", ctrl::output_path.c_str());
+				extracted.output_path = current_arg;
+				log_info("Usando `%s` como caminho de saida\n", extracted.output_path.c_str());
 			} else {
-				ctrl::output_path = "./output/";
-				log_info("Nenhum valor passado para a opcao `-o`, usando `%s` como caminho de saida\n", ctrl::output_path.c_str());
+				extracted.output_path = "./output/";
+				log_info("Nenhum valor passado para a opcao `-o`, usando `%s` como caminho de saida\n", extracted.output_path.c_str());
 			}
 		}
 	}
+
+	return extracted;
 }
