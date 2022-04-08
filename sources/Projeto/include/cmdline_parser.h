d89d87e921f5ce98b643f08d03d4be5f07e7152e
Fabricio Milanez <fabriciomilanez@protonmail.com>
2022-04-08 15:02:44 -0300

    reverted usage back to accepting a directory containing all the dataset images instead of a file listing them

--- sources/Projeto/include/cmdline_parser.h
+++ sources/Projeto/include/cmdline_parser.h
@@ -17,14 +17,14 @@ namespace ctrl {
 /** Armazena os argumentos validados extraídos da linha de comando em variáveis de controle. */
 struct args {
 	// índices dos argumentos da linha de comando na lista de argumentos
-	static const int INPUT_FILE_IDX 	= 1; 	/** Índice do argumento orbigatório <input_file> na lista de argumentos. */
+	static const int INPUT_PATH_IDX 	= 1; 	/** Índice do argumento orbigatório <input_path> na lista de argumentos. */
 	static const int FOCAL_LENGTH_IDX 	= 2; 	/** Índice do argumento orbigatório <f> na lista de argumentos. */
 	static const int PRINCIPAL_X_IDX 	= 3; 	/** Índice do argumento orbigatório <cx> na lista de argumentos. */
 	static const int PRINCIPAL_Y_IDX 	= 4; 	/** Índice do argumento orbigatório <cy> na lista de argumentos. */
 	static const int MIN_ARGS 			= 5; 	/** Número mínimo de argumentos. Também representa o índice do primeiro argumento subsequente aos obrigatórios. */
 
 	// caminhos de entrada e saída
-	fs::path input_file{}; 				/* Caminho até o arquivo que lista os caminhos das imagens do dataset. */
+	fs::path input_path{}; 				/* Caminho até o diretório de entrada contendo as imagens do dataset. */
 	fs::path output_path{"./output"}; 	/* Caminho até o diretório de saída. */
 
 	// Parâmetros intrínsecos da câmera
--- sources/Projeto/src/cmdline_parser.cpp
+++ sources/Projeto/src/cmdline_parser.cpp
@@ -17,7 +17,7 @@ namespace fs = std::filesystem; // filesystem namespace alias
 /********** Macros **********/
 
 // macros para acesso à índices específicos da lista de argumentos
-#define ARG_INPUT_FILE 		argv[ctrl::args::INPUT_FILE_IDX]
+#define ARG_INPUT_PATH 		argv[ctrl::args::INPUT_PATH_IDX]
 #define ARG_FOCAL_LENGTH 	argv[ctrl::args::FOCAL_LENGTH_IDX]
 #define ARG_CX 				argv[ctrl::args::PRINCIPAL_X_IDX]
 #define ARG_CY 				argv[ctrl::args::PRINCIPAL_Y_IDX]
@@ -32,16 +32,15 @@ namespace fs = std::filesystem; // filesystem namespace alias
 /** Insere a mensagem de ajuda do programa em um objeto std::ostream (std::cout por padrão). */
 void ctrl::help(std::ostream& os) {
 	os << 
-		"Uso: ./Projeto <input_file> <f> <cx> <cy> [OPTIONS]\n" 
+		"Uso: ./Projeto <input_path> <f> <cx> <cy> [OPTIONS]\n" 
 		"    Computa uma nuvem de pontos esparsa para um conjunto de imagens.\n" 
 		"    Ao final da execucao, dois arquivos serao gerados: um arquivo .obj contendo\n" 
 		"    apenas a lista de vertices, representando a nuvem de pontos, e um arquivo\n" 
 		"    .sfm detalhando a pose (matrix de transformacao Rt) estimada para cada\n" 
 		"    camera.\n" 
 		"\n" 
-		"<input_file>:\n" 
-		"    Arquivo contendo os caminhos absolutos ate as imagens do dataset a serem\n" 
-		"    usadas, um por linha.\n" 
+		"<input_path>:\n" 
+		"    Caminho ate o diretorio contendo as imagens do dataset.\n"
 		"<f>:\n" 
 		"    Distancia Focal da camera.\n" 
 		"<cx> e <cy>:\n" 
@@ -77,9 +76,9 @@ ctrl::args ctrl::parse(const int argc, const char* const* argv) {
 	// instanciando variavés de controle com valores padrão
 	ctrl::args extracted{};
 
-	// extraindo e validando argumento obrigatório <input_file>
-	if (extracted.input_file = ARG_INPUT_FILE; !fs::exists(extracted.input_file) || !fs::is_regular_file(extracted.input_file)) {
-		log_error_and_exit("Arquivo de entrada `%s` nao existe ou nao e valido\n", ARG_INPUT_FILE);
+	// extraindo e validando argumento obrigatório <input_path>
+	if (extracted.input_path = ARG_INPUT_PATH; !fs::exists(extracted.input_path) || !fs::is_regular_file(extracted.input_path)) {
+		log_error_and_exit("Arquivo de entrada `%s` nao existe ou nao e valido\n", ARG_INPUT_PATH);
 	}
 
 	// extraindo e validando argumento obrigatório <f>
