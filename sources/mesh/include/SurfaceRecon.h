8e2719ed47042fa231f1dd5e16f4645de14bcc9c
Fabricio Milanez <fabriciomilanez@protonmail.com>
2022-05-19 09:59:35 -0300

    added target mesh

---
+++ sources/mesh/include/SurfaceRecon.h
@@ -0,0 +1,4 @@
+#include <pcl-1.12/pcl/point_types.h>
+#include <pcl-1.12/pcl/io/pcd_io.h>
+#include <pcl-1.12/pcl/io/obj_io.h>
+#include <pcl-1.12/pcl/io/ply_io.h>
---
+++ sources/mesh/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+	std::cout << "./mesh: not implemented\n";
+	return 0;
+}
